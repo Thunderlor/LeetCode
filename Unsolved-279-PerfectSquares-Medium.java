ee537696e08f037432b0366a4e97076377ff59b7
OuYangQiQi <838398078@qq.com>
2015-09-27 22:49:54 +0800

    Update and rename 279-PerfectSquares-Medium.java to Unsolved-279-PerfectSquares-Medium.java

--- Unsolved-279-PerfectSquares-Medium.java
+++ Unsolved-279-PerfectSquares-Medium.java
@@ -5,8 +5,15 @@
  * given n = 13, return 2 because 13 = 4 + 9.
  */
  
- 
- 
+public class Solution {
+    /* 思路二：列了一个表，发现似乎最多只需要四个平方和就能表示任意一个自然数 
+     * 因此查了查百度，发现这是数论里的四平方和定理
+     * Lagrange's Four-Square Theorem：所有自然数至多只要用四个数的平方和就可以表示
+     */
+    public int numSquares(int n) {
+    
+    }
+}
 //-----------------------------------------
 public class Solution {
     /* 思路一：迭代求取目标数所含最大的Pefect Square Number */
